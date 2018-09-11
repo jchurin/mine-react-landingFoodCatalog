3decc3e80da5ea4da272fc358b104764e7e5f414
jchurin95 <juanchurin@gmail.com>
2018-09-10 23:52:53 -0300

    Se brinda el color en dos variables, y una de contraste, para que sea mas sencillo cambiar el look and feel de la pagina

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-// CSS imports
+// ********************* CSS *********************
 import './assets/css/bootstrap.min.css';
 import './assets/css/font-awesome.min.css';
 import './assets/css/owl.carousel.css';
 import './assets/css/owl.theme.css';
-import './assets/css/animate.css';
 import './assets/css/flexslider.css';
-import './assets/css/pricing.css';
-import './assets/css/main.css';
-import './index.scss';
+import './assets/css/animate.css';
 
-// JS imports
+import './assets/css/index.scss';
+
+// ********************* JS *********************
 import jquery from 'jquery';
 import $ from 'jquery';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// ********************* REQUIRE *********************
 window.$ = window.jQuery = jquery;
 require('./assets/js/jquery.flexslider.min');
 require('./assets/js/bootstrap.min');
@@ -26,7 +25,6 @@ require('./assets/js/owl.carousel.min');
 require('./assets/js/jquery.mixitup');
 require('./assets/js/wow.min');
 require('./assets/js/jquery.validate');
-// require('./assets/js/jquery.hoverdir');
 require('./assets/js/jQuery.scrollSpeed');
 require('./assets/js/script');
 
